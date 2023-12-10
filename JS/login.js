94bc24220b2868d9127a407f5642f12460675bcb
reyag10 <mdreyazuddin632@gmail.com>
2023-12-10 22:23:36 +0600

    login page design 50% done

---
+++ JS/login.js
@@ -0,0 +1,1 @@
+console.log("The International Cricket Council is the global governing body of cricket. It was founded as the Imperial Cricket Conference in 1909 by representatives from Australia, England, and South Africa. It was renamed as the International Cricket Conference in 1965 and adopted its current name in 1987");
